bd694dbb00a9053e1d4810ac3e4f1de5c9b14b67
SherifCaptainRed <anshtrips07@gmail.com>
2024-12-02 19:12:01 +0530

    First Commit

---
+++ 01-Beginner/Day-01/app.js
@@ -0,0 +1,37 @@
+// Day 1: Hello World in JavaScript
+
+/*
+ * Introduction:
+ * This project is the very first step in your 45-day coding challenge.
+ * The "Hello, World!" program is a simple yet significant milestone for any programmer.
+ * It is often the first program written when learning a new programming language because it provides a basic introduction to the syntax and structure of the language.
+ * In this project, you will create a "Hello, World!" program in JavaScript.
+ * This will help you get familiar with the development environment and basic syntax of JavaScript.
+ */
+
+// Using innerHTML to write into an HTML element
+// This method sets or returns the HTML content of an element
+// document.getElementById("elementID").innerHTML = "Hello, World!";
+document.getElementById("elementID").innerHTML = "Hello, World!";
+
+function hoverText(x) {
+  document.getElementById("elementID").innerHTML = "Written By JS";
+}
+
+function hoverDownText(x) {
+  document.getElementById("elementID").innerHTML = "Hello, World!";
+}
+
+// Using window.alert() to display an alert box with a message
+// This method displays an alert dialog with the specified message and an OK button
+// window.alert("Hello, World!");
+
+window.alert(
+  "This is the first code of the challenge, Just hover over the text"
+);
+
+// Using console.log() to write into the browser console
+// This method outputs a message to the web console
+// console.log("Hello, World!");
+
+console.log("Prety Impressive. RIGHT!??");
