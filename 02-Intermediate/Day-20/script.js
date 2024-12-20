177d624c1c1f20eaccef60e3a53bcb3900a20b03
SherifCaptainRed <anshtrips07@gmail.com>
2024-12-20 17:20:38 +0530

    day 20

---
+++ 02-Intermediate/Day-20/script.js
@@ -0,0 +1,32 @@
+async function findRecipes() {
+  const ingredientInput = document.getElementById("ingredient").value;
+  const apiKey = "a831ae409c9442e9a651ee5e8e3b548d";
+  const apiUrl = `https://api.spoonacular.com/recipes/findByIngredients?ingredients=${ingredientInput}&number=10&apiKey=${apiKey}`;
+
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    displayRecipes(data);
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+  }
+}
+
+function displayRecipes(recipes) {
+  const recipesContainer = document.getElementById("recipes");
+  recipesContainer.innerHTML = "";
+
+  recipes.forEach((recipe) => {
+    const recipeElement = document.createElement("div");
+    recipeElement.innerHTML = `
+          <h2>${recipe.title}</h2>
+          <img src="${recipe.image}" alt="${recipe.title}">
+          <p>Used Ingredients: ${recipe.usedIngredientCount}</p>
+          <p>Missed Ingredients: ${recipe.missedIngredientCount}</p>
+          <a href="https://spoonacular.com/recipes/${recipe.title
+            .replace(/\s+/g, "-")
+            .toLowerCase()}-${recipe.id}" target="_blank">View Recipe</a>
+      `;
+    recipesContainer.appendChild(recipeElement);
+  });
+}
